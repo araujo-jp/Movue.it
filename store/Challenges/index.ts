a4e532b4d826b178ac041cdc250da93dfab9617c
Joao Paulo Araujo <araujojp@protonmail.com>
2021-08-25 11:11:53 -0400

    feat: add challenges module

---
+++ store/Challenges/index.ts
@@ -0,0 +1,20 @@
+import allChallenges from '../../assets/challenges/data';
+
+import { State } from './types';
+
+export const state = (): State => ({
+	level: 1,
+	xp: {
+		current: 0,
+		start: 0,
+		end: 64,
+	},
+	completedChallenges: 0,
+	currentChallengeIndex: null,
+	allChallenges,
+	isLevelUpModalOpen: false,
+});
+
+
+
+
